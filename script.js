70ad81dc2bcab5c2162a62478871e973715c78f6
charantadakala <charantejatadakala21@gmail.com>
2024-10-29 14:19:13 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-// JavaScript to fade out the greeting message after 3 seconds
-window.onload = function() {
-    setTimeout(() => {
-        document.getElementById("greetingMessage").style.display = "none";
-    }, 3000);
-};
+/* style.css */
+body, html {
+  margin: 0;
+  padding: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: black;
+  color: white;
+  font-family: Arial, sans-serif;
+  font-size: 2em;
+}
+
+#greeting {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
